f0db09a487f3a08fe6be54faf2cfe3d7cb550746
Jonathan Brierre <github email address>
2019-12-31 17:56:51 -0500

    colored links & added github to about

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,5 +1,5 @@
 class ItemsController < ApplicationController
-    skip_before_action :authorized, only:[:index, :show, :shirts, :pants, :hats, :about]
+    skip_before_action :authorized, only:[:index, :shirts, :pants, :hats, :about]
 
     def index
         @items = Item.all
